67b94db5841e9bfbac7d6b4c7bd9a4f30115241a
normalek <filmacker@gmail.com>
2017-11-22 01:12:00 +0300

    MVC extension

--- src/main/java/com/learning/utils/DictionaryRepository.java
+++ src/main/java/com/learning/utils/DictionaryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called dictionaryRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface DictionaryRepository extends CrudRepository<Dictionary, Long> {
+public interface DictionaryRepository extends CrudRepository<Dictionary, Integer> {
 
 }
